7f1f5a52411cd5f77dc2761d643a659c40976db4
Ryan Keller <rykeller@uw.edu>
2019-05-19 19:40:34 -0600

    Initial commit based on orb-tools template

---
+++ README.md
@@ -0,0 +1,1 @@
+# CircleCI Runtime Optimization Orb [![CircleCI status](https://circleci.com/gh/rckeller/runtime-optimizer.svg "CircleCI status")](https://circleci.com/gh/rckeller/runtime-optimizer) [![CircleCI Orb Version](https://img.shields.io/badge/endpoint.svg?url=https://badges.circleci.io/orb/rckeller/runtime-optimizer)](https://circleci.com/orbs/registry/orb/rckeller/runtime-optimizer)
---
+++ src/scripts/check-changes.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+# https://circleci.com/docs/2.0/using-shell-scripts/#set-error-flags
+# Exit script if you try to use an uninitialized variable.
+set -o nounset
+# Exit script if a statement returns a non-true return value.
+set -o errexit
+# Use the error status of the first failure, rather than that of the last item in a pipeline.
+set -o pipefail
+
+range() {
+    echo "$1" | rev | cut -d/ -f1 | rev
+}
+
+last_run_commit() {
+    echo "$1" | cut -d. -f 1 | cut -d^ -f 1
+}
+
+current_commit() {
+    echo "$1" | cut -d. -f 4
+}
+
+compare_range=$(range $1)
+first=$(current_commit $compare_range)
+last=$(last_run_commit $compare_range)
+
+echo "Comparing changes for range ${last} to ${first} from URL ${1}:"
+
+output=$(git diff $last^ $first -- $2 2>&1)
+
+if [ $? -gt 0 ]; then
+  echo "Error looking for changes!"
+  exit 0
+elif [ -n "$output" ]; then
+  echo "Changes detected - proceeding"
+  exit 0
+else
+  echo "No changes detected - halting job"
+  exit 1
+fi
