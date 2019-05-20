7ff09fd9da72ccbd09b08dfe6fb4edc5971bdfc8
Ryan Keller <rykeller@uw.edu>
2019-05-20 16:53:09 -0600

    Remove uninitialized fail flags

--- src/scripts/check-changes.sh
+++ src/scripts/check-changes.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
 # https://circleci.com/docs/2.0/using-shell-scripts/#set-error-flags
-# Exit script if you try to use an uninitialized variable.
-set -o nounset
-# Exit script if a statement returns a non-true return value.
-# set -o errexit
-# Use the error status of the first failure, rather than that of the last item in a pipeline.
+# # Use the error status of the first failure, rather than that of the last item in a pipeline.
 set -o pipefail
 
 range() {
